6d97ab81be51fec59ec84883ff92bd7ded95606c
Oscar Wilfren <wilfrenmoney@gmail.com>
2023-10-20 22:37:54 -0500

    advance

--- src/components/Phrase.jsx
+++ src/components/Phrase.jsx
@@ -5,7 +5,7 @@ const Phrase = ({phraseRandom}) => {
   return (
     <article className="div__article">
         <p className="div__p">{phraseRandom['phrase']}</p>
-        <img className="div__img--rectangle" src="/src/assets/images/rectangle1.svg" alt="rectangle"/>
+        <img className="div__img--rectangle" src="../../rectangle1.svg" alt="rectangle"/>
     </article>
     
   )
